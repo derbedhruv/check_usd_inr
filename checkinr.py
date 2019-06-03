44769e5140d04a5e281808bc60c902d47d0cafd1
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2019-06-02 21:49:12 -0700

    Add basic requet to xoom and parsing INR

---
+++ checkinr.py
@@ -0,0 +1,14 @@
+# check if there's a high value of INR going on
+# can check as global and local maxima
+import requests
+
+XOOM_LINK = 'https://www.xoom.com/india/send-money'
+
+response = requests.get(XOOM_LINK)
+loc = response.text.find('1 USD =')
+CURRENT_USD_INR_VALUE = response.text[loc+8: loc+15]
+
+print("current USD = " + CURRENT_USD_INR_VALUE)
+
+if (CURRENT_USD_INR_VALUE >= 70.0):
+  # send an email saying that it is so
